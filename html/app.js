aefc8473cfdf984e3042b7c64c7c301be32514af
iago holek <87425168+FunctionTestIfImGood@users.noreply.github.com>
2022-07-27 13:36:55 -0300

    Add files via upload

---
+++ html/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+  document.body.classList.toggle('light-theme');
+  document.body.classList.toggle('dark-theme');
+ 
+  const className = document.body.className;
+   
+  switch (className){
+    case "light-theme": this.textContent = "Dark";break;
+    case "dark-them": this.textContent = "Light"; break;
+  }
+
+  console.log('classe atual: ' + className);
+});
